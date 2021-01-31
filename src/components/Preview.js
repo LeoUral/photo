3e19a1fd3ef2b1a1728c41d66ac9d7c973ab2f42
Leonid <leoural66@yandex.ru>
2021-01-31 16:04:03 +0500

    create preview block

---
+++ src/components/Preview.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+//Превью альбомов с фотографией
+export default class Preview extends React.Component {
+
+    render() {
+        return (
+            <>
+            </>
+        )
+    }
+}
