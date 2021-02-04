9d8fd8e048da16382a7d8587f9b135d038928d33
Leonid <leoural66@yandex.ru>
2021-02-04 22:05:03 +0500

    Начало формирования компонента модального окна

--- src/components/ModalWindow.js
+++ src/components/ModalWindow.js
@@ -4,8 +4,23 @@ import React from 'react';
 export default class ModalWindow extends React.Component {
 
     render() {
+        const i = 0;
+        const j = 0;
+        const url = `../image/gal_${i}/img_${j}.jpg`
+
         return (
             <>
+                <section className="modal">
+                    <div className="modal__shadow"></div>
+                    <div className="modal__window">
+                        <button className="btn__close"> close </button>
+                        <div className="modal__block">
+                            <button className="btn__left"> left </button>
+                            <div className="modal__image" style={{ backgroundImage: `url(${url})` }}></div>
+                            <button className="btn__right"> right </button>
+                        </div>
+                    </div>
+                </section>
             </>
         )
     }
