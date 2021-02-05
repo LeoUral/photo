68f279fbbb0b02778b32953e99b4e1eca1bb77ef
Leonid <leoural66@yandex.ru>
2021-02-05 12:38:57 +0500

    Update ModalWindow.js

--- src/components/ModalWindow.js
+++ src/components/ModalWindow.js
@@ -2,22 +2,42 @@ import React from 'react';
 
 // вывод фото для просмотра из выбранной галереи
 export default class ModalWindow extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: false
+        }
+        this.handleClose = this.handleClose.bind(this);
+        this.handleStepLeft = this.handleStepLeft.bind(this);
+        this.handleStepRight = this.handleStepRight.bind(this);
+    }
+
+    handleStepLeft() {
+        console.log('STEP LEFT');
+    }
+    handleStepRight() {
+        console.log('STEP RIGHT');
+    }
+    handleClose() {
+        console.log('CLOSE');
+    }
 
     render() {
         const i = 0;
         const j = 0;
         const url = `../image/gal_${i}/img_${j}.jpg`
+        const show = this.state.show;
 
         return (
             <>
-                <section className="modal">
-                    <div className="modal__shadow"></div>
+                <section className="modal" style={{ display: show ? 'block' : 'none' }}>
+                    <div className="modal__shadow" onClick={this.handleClose}></div>
                     <div className="modal__window">
-                        <button className="btn__close"> close </button>
+                        <button className="btn--close" onClick={this.handleClose}> close </button>
                         <div className="modal__block">
-                            <button className="btn__left"> left </button>
+                            <button className="btn--left" onClick={this.handleStepLeft}> left </button>
                             <div className="modal__image" style={{ backgroundImage: `url(${url})` }}></div>
-                            <button className="btn__right"> right </button>
+                            <button className="btn--right" onClick={this.handleStepRight}> right </button>
                         </div>
                     </div>
                 </section>
